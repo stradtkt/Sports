ddd59a10a0a7844ecd19ea768e88f20220ad71f1
Kevin Stradtman <stradtkt@gmail.com>
2018-12-15 20:31:23 -0500

    Added the login register views with the user model

--- apps/drego/models.py
+++ apps/drego/models.py
@@ -1,3 +1,52 @@
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 from django.db import models
+import datetime
 
-# Create your models here.
+class UserManager(models.Manager):
+    def validate_user(self, postData):
+        errors = {}
+        # validate first and last name
+        if len(postData['first_name']) < 2 or not postData['first_name'].isalpha():
+            if len(postData['first_name']) < 2:
+                errors['first_len'] = "Your first name needs to be two or more characters"
+            if not postData['first_name'].isalpha():
+                errors['first_alpha'] = "Your first name needs to be only letters"
+        if len(postData['last_name']) < 2 and not postData['last_name'].isalpha():
+            if len(postData['last_name']) < 2:
+                errors['last_len'] = "Your last name needs to be two or more characters"
+            if not postData['last_alpha'].isalpha():
+                errors['last_alpha'] = "Your last name needs to be only letters"
+        
+        #Validate DOB
+        if str(postData['DOB']) > str(datetime.datetime.now):
+            errors['DOB'] = "The date of birth cannot be in the future"
+        elif str(postData['DOB']) == str(datetime.datetime.now):
+            errors['DOB'] = "The date of birth cannot be right now"
+        # validate email
+        try:
+            validate_email(postData['email'])
+        except ValidationError:
+            errors['email'] = "Your email is not valid"
+        else:
+            if User.objects.filter(email=postData['email']):
+                errors['email'] = "This email already exists"
+
+        # validate password
+        if len(postData['password']) < 8:
+            errors['password'] = "Please enter a longer password, needs to be 8 or more characters"
+        if postData['password'] != postData['confirm_pass']:
+            errors['confirm_pass'] = "Passwords must match"
+        return errors
+            
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    DOB = models.DateField()
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
+    def __str__(self):
+        return first_name + " " + last_name
--- apps/drego/views.py
+++ apps/drego/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'drego/index.html', context)
+
+def login(request):
+    email = request.POST['email']
+    password = request.POST['password']
+    user = User.objects.filter(email=email)
+    if len(user) > 0:
+        is_pass = bcrypt.checkpw(password.encode('utf-8'), user[0].password.encode('utf-8'))
+        if is_pass:
+            request.session['id'] = user[0].id
+            return redirect('/dashboard')
+        else:
+            messages.error(request, "Incorrect email and/or password")
+            return redirect('/')
+    else:
+        messages.error(request, "User does not exist")
+    return redirect('/')
+
+def register(request):
+    errors = User.objects.validate_user(request.POST)
+    if len(errors):
+        for tag, error in errors.items():
+            messages.error(request, error)
+        return redirect('/')
+    else:
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        DOB = request.POST['DOB']
+        password = request.POST['password']
+        hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        User.objects.create(first_name=first_name, last_name=last_name, email=email, DOB=DOB, password=hashed_pw)
+        return redirect('/')
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+
+def register_page(request):
+    return render(request, 'drego/register.html')
+
+def login_page(request):
+    return render(request, 'drego/login.html')
